a1daf24ed83fe28556beef459f1395e347cd74cd
mikroby <rtamas@hotmail.com>
2021-10-14 17:06:23 +0200

    task solved

---
+++ extra-feladatok-balloons/app.js
@@ -0,0 +1,25 @@
+let numberOfBalloons;
+
+function addMouseEvent() {
+    balloons = document.querySelectorAll(".balloon");
+
+    numberOfBalloons = balloons.length;
+
+    for (i = 0; i < numberOfBalloons; i++) {
+        balloons[i].onmouseover = function () { popThat(this) };
+    }
+}
+
+function popThat(element) {
+    element.onmouseover = null;
+    element.setAttribute("style", "background: #ededed");
+    element.innerHTML = "POP!";
+
+    numberOfBalloons--;
+
+    if (numberOfBalloons == 0) {
+        document.querySelector("#yay-no-balloons").setAttribute("style", "display: block");
+    }
+}
+
+addMouseEvent();
