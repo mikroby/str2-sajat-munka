a249b5a4ea61df41332d61729309f594c3a58076
mikroby <rtamas@hotmail.com>
2021-10-15 02:11:03 +0200

    task solved

--- extra-feladatok-height-converter/app.js
+++ extra-feladatok-height-converter/app.js
@@ -2,19 +2,20 @@ const RATIO = 2.54;
 document.querySelector("#calculator").addEventListener("submit", function (ev) {
     ev.preventDefault(); validate();
 });
+let results = document.querySelector("#results");
 
 function validate() {
     let inches = parseFloat(document.querySelector("#inches").value);
     let feet = parseFloat(document.querySelector("#feet").value);
 
     if (isNaN(inches) || isNaN(feet) || inches < 0 || feet < 0) {
-        document.querySelector("#results").innerHTML = "Enter values >= 0, please !";
+        results.innerHTML = "Enter values >= 0, please !";
         return;
     }
-    
+
     calculate(inches, feet);
 }
 
 function calculate(inches, feet) {
-    document.querySelector("#results").innerHTML = `${(feet * 12 + inches) * RATIO}`;
+    results.innerHTML = `${(feet * 12 + inches) * RATIO}`;
 }
